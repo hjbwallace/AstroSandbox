2c65e65b7e3c8af689dcff23040e6d45bfe152b7
Harrison Wallace <hjbwallace@gmail.com>
2025-06-03 20:17:53 +0800

    Adding sitemap and programatic robots
    https://docs.astro.build/en/guides/integrations-guide/sitemap/

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import preact from '@astrojs/preact';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hjbwallace.github.io',
   base: '/AstroSandbox',
-  integrations: [preact()],
+  integrations: [
+    preact(), 
+    sitemap({
+      filter: (page) => page !== 'https://hjbwallace.github.io/AstroSandbox/posts/post-1/',
+      entryLimit: 10000,
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date('2022-02-24'),
+    }),
+  ],
 });
